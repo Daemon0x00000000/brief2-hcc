13df25a03231c8bc4a50ce37ce3287c175a88766
Dimitri <dimitri@livin-france.com>
2024-02-22 10:34:20 +0100

    structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataModule } from './data/data.module';
+import { PresentationModule } from './presentation/presentation.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DataModule, PresentationModule, DomainModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
